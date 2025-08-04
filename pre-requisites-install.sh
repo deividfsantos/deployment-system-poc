ee6b9d8d2c52d083599d684ec277283b22a06690
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-08-04 01:02:26 -0300

    start deployment system poc

---
+++ pre-requisites-install.sh
@@ -0,0 +1,11 @@
+# Install minikube
+# curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+# sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+# # Install Kubectl
+# # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+
+# sudo usermod -aG docker $USER && newgrp docker
+
+# Install OpenTofu
+# https://opentofu.org/docs/intro/install/deb/
---
+++ setup-cluster.sh
@@ -0,0 +1,24 @@
+
+echo "Starting minikube cluster" 
+minikube start \
+    --driver=docker \
+    --cpus=4 \
+    --memory=8192 \
+    --disk-size=5GB \
+    --kubernetes-version=v1.33.0
+
+kubectl create namespace infrastructure --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace applications --dry-run=client -o yaml | kubectl apply -f -
+
+minikube addons enable ingress
+minikube addons enable metrics-server
+minikube addons enable dashboard
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add jenkins https://charts.jenkins.io
+helm repo add stable https://charts.helm.sh/stable
+helm repo update
+
+kubectl get nodes
+kubectl get pods --all-namespaces
