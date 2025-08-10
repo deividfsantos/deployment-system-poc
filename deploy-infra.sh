d830883601ff2501e41476bf67c3040df989f542
Deivid Santos <deividfranciscosantos@hotmail.com>
2025-08-09 23:45:28 -0300

    fix jenkinsfile issue

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -5,3 +5,10 @@ tofu validate
 tofu apply -auto-approve
 echo "Infrastructure deployment completed"
 tofu output
+
+kubectl create secret docker-registry regcred \
+    --docker-server=https://index.docker.io/v1/ \
+    --docker-username=deividsantos \
+    --docker-password=<token> \
+    --docker-email=deividfranciscosantos@hotmail.com \
+    -n infrastructure
